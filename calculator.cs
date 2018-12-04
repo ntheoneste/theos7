92ac60343909f484a5853dbb5f9710856fb2bca9
ntheoneste <45235265+ntheoneste@users.noreply.github.com>
2018-12-04 15:58:24 +0200

    third commit
    thirdcommit

---
+++ calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progam
+{
+    class calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new Exception("Num 2 should not be equal to zero");
+            }
+
+            return num1 / num2;
+
+
+        }
+    }
+}
